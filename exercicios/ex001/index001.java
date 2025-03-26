73558cc90e11d46da5db0bc5da5c1f7e1c06d8a7
caio-sdm <marinhocaio1308@gmail.com>
2025-03-25 23:10:32 -0300

    ex001 (ódio genuíno)

---
+++ exercicios/ex001/index001.java
@@ -0,0 +1,14 @@
+package exercicios.ex001;
+
+public class index001 {
+    public static void main (String[] args){
+        double N1, N2, N3, N4, RES;
+        N1 = Double.parseDouble(System.console().readLine());
+        N2 = Double.parseDouble(System.console().readLine());
+        N3 = Double.parseDouble(System.console().readLine());
+        N4 = Double.parseDouble(System.console().readLine());
+
+        RES = (N1+N2+N3+N4)/4;
+        System.out.println("Sua média é: " + RES);
+    }
+}
