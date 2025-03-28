d46d0286d846542e20c3bdf65b997a61f519edf8
caio-sdm <marinhocaio1308@gmail.com>
2025-03-28 15:43:12 -0300

    ex001

--- exercicios/ex001/ex001.java
+++ exercicios/ex001/ex001.java
@@ -1,12 +1,16 @@
 public class ex001 {
     public static void main (String[] args){
         double N1, N2, N3, N4, RES;
+        System.out.print("Digite sua primeira nota: ");
         N1 = Double.parseDouble(System.console().readLine());
+        System.out.print("Digite sua segunda nota: ");
         N2 = Double.parseDouble(System.console().readLine());
+        System.out.print("Digite sua terceira nota: ");
         N3 = Double.parseDouble(System.console().readLine());
+        System.out.print("Digite sua quarta nota: ");
         N4 = Double.parseDouble(System.console().readLine());
 
         RES = (N1+N2+N3+N4)/4;
-        System.out.println("Sua média é: " + RES);
+        System.out.printf("Sua média é: %.1f\n", RES);
     }
 }
