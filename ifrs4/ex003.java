a59ca76c9abc48b39c01a3c79af1ce2cb9d90834
caio-sdm <marinhocaio1308@gmail.com>
2025-04-30 23:39:26 -0300

    ex003

--- ifrs4/ex003.java
+++ ifrs4/ex003.java
@@ -1,4 +1,14 @@
 package ifrs4;
 
 public class ex003 {
+    public static void main(String[]args){
+        int n;
+        String bin;
+
+        System.out.print("Escreva um número em binário: ");
+        bin = System.console().readLine();
+
+        n = Integer.parseInt(bin, 2);
+        System.out.printf("'%s' representa '%d' em decimal", bin,n);
+    }
 }
