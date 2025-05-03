35643a6c0709b3c7823d0499ef0f428774fd7372
caio-sdm <marinhocaio1308@gmail.com>
2025-05-02 23:20:20 -0300

    ex006

---
+++ ifrs4/ex006.java
@@ -0,0 +1,12 @@
+package ifrs4;
+
+public class ex006 {
+    public static void main(String[]args){
+        int dado;
+
+        dado = (int)(1 + Math.random()*6);
+
+        System.out.printf("O número sorteado foi: %d",dado);
+
+    }
+}
