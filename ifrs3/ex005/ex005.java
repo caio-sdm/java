49fd3215e2911bf2af79b27f9e527a6d8794bd20
caio-sdm <marinhocaio1308@gmail.com>
2025-05-03 00:18:10 -0300

    ex005 006 e 007

--- ifrs3/ex005/ex005.java
+++ ifrs3/ex005/ex005.java
@@ -1,4 +1,35 @@
 package ifrs3.ex005;
 
 public class ex005 {
+    public static void main (String[]args){
+        double litro, dist, consumo, somaconsumo, media;
+        int viagens, i;
+
+        somaconsumo = 0;
+
+        System.out.println("Para encerrar, digite 0 \n");
+
+        System.out.print("Quantas viagens foram feitas? ");
+        viagens = Integer.parseInt(System.console().readLine());
+
+        if (viagens == 0){
+            System.out.print("\nNão foram feitas viagens :D");
+        }
+
+        if (viagens > 0){
+            for (i = 1; i <= viagens; i++){
+                System.out.printf("\nInforme quantos litros gastos na viagem %d: ",i);
+                litro = Double.parseDouble(System.console().readLine());
+
+                System.out.printf("Informe quantos quilômetros rodados na viagem %d: ",i);
+                dist = Double.parseDouble(System.console().readLine());
+
+                consumo = dist/litro;
+                somaconsumo += consumo;
+            }
+        }
+
+        media = somaconsumo / viagens;
+        System.out.printf("\nO consumo médio foi: %.2f litros",media);
+    }
 }
---
+++ ifrs3/ex007/ex007.java
@@ -0,0 +1,15 @@
+package ifrs3.ex007;
+
+public class ex007 {
+    public static void main (String[]args){
+        int numero, quadrado, i;
+        numero = 15;
+
+        for (i = 15; i <= 200; i++){
+            quadrado = numero * numero;
+            System.out.printf("O quadrado de %d é igual a %d\n",numero,quadrado);
+            numero++;
+        }
+        System.out.print("\nAcabou :D");
+    }
+}
