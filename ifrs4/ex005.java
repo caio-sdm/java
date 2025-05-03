f73944ffdc2f39dbf9fecb3d190517af8df91f1a
caio-sdm <marinhocaio1308@gmail.com>
2025-05-02 23:06:37 -0300

    ex005

--- ifrs4/ex005.java
+++ ifrs4/ex005.java
@@ -2,7 +2,7 @@ package ifrs4;
 
 public class ex005 {
     public static void main (String[]args){
-        double n;
+        double n, fator, nfinal;
         int casas;
 
         System.out.print("Escreva um número: ");
@@ -10,6 +10,9 @@ public class ex005 {
         System.out.print("Escreva quantas casas decimais o número sera arredondado (para cima): ");
         casas = Integer.parseInt(System.console().readLine());
 
-        
+        fator = Math.pow(10, casas);
+        nfinal = Math.round(n * fator)/fator;
+
+        System.out.printf("Número original: %f\nNúmero arredondado: %f",n,nfinal);
     }
 }
