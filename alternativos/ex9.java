a068d03a4d08f3f32df3dccb4d7a00b9b7ea95ff
caio-sdm <marinhocaio1308@gmail.com>
2025-05-21 00:13:33 -0300

    ex9

---
+++ alternativos/ex9.java
@@ -0,0 +1,10 @@
+package alternativos;
+
+public class ex9 {
+    public static void main(String[]args){
+        int i = 0;
+        do{
+            System.out.print(i++);
+        } while (i<10);
+    }
+}
