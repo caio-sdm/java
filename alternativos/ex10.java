cc844449fc9d0d5c2a7f4bcc0cb682e517812fcc
caio-sdm <marinhocaio1308@gmail.com>
2025-06-04 22:33:38 -0300

    ex10

---
+++ alternativos/ex10.java
@@ -0,0 +1,11 @@
+package alternativos;
+
+public class ex10 {
+    public static void main(String[]args){
+        int[] numeros = new int[10];
+        int cont;
+        boolean achou;
+
+        
+    }
+}
