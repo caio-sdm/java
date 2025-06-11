50c43e1381bda90b7fa692ff3ca5eeae9985f8c3
caio-sdm <marinhocaio1308@gmail.com>
2025-06-11 09:54:17 -0300

    ex005

--- ex/ifrs1/ex005.java
+++ ex/ifrs1/ex005.java
@@ -4,17 +4,18 @@ média da idade, media de peso e altura, porcentagem feminino, porcentagem mascu
 public class ex005 {
     public static void main(String[]args){
         char sexo, corcabelo, continuar;
-        int corolhos, idade=0, somaidades=0, azulruivo=0, pessoas=1, homens=0, mulheres=0;
+        int corolhos, idade, somaidades=0, azulruivo=0, pessoas=0, homens=0, mulheres=0;
         double altura, peso, mediaidade, mediapeso, somapeso=0, medialtura, somaltura=0, porcentmasc, porcentfem;
 
-        System.out.println("Começar cadastros? (S/N)\n");
+        System.out.println("Começar cadastros? (S/N)");
         continuar = System.console().readLine().charAt(0);
         System.out.print("Cor dos olhos: \n1-Azuis \n2-Verdes \n3-Castanhos\n");
-        System.out.print("Cor dos cabelos: \nL-Loiros \nR-Ruivos \nC-Castanhos \nP-Pretos");
+        System.out.print("Cor dos cabelos: \nL-Loiros \nR-Ruivos \nC-Castanhos \nP-Pretos\n");
 
         while (continuar == 'S' || continuar == 's'){
-            System.out.printf("Pessoa %d",pessoas);
-            System.out.print("===========");
+            pessoas++;
+            System.out.printf("\nPessoa %d\n",pessoas);
+            System.out.print("===========\n");
 
             System.out.print("Sexo (M/F): ");
             sexo = System.console().readLine().charAt(0);
@@ -43,15 +44,23 @@ public class ex005 {
             altura = Double.parseDouble(System.console().readLine());
             somaltura += altura;
 
-            System.out.print("Peso; ");
+            System.out.print("Peso: ");
             peso = Double.parseDouble(System.console().readLine());
             somapeso += peso;
 
-
-
-            pessoas++;
-            System.out.println("Cadastrar outra pessoa? (S/N)");
+            System.out.println("\nCadastrar outra pessoa? (S/N)");
             continuar = System.console().readLine().charAt(0);
         }
+        porcentfem = (double) (100*mulheres)/pessoas;
+        porcentmasc = (double) (100*homens)/pessoas;
+        mediaidade = (double) somaidades/pessoas;
+        mediapeso = somapeso/pessoas;
+        medialtura = somaltura/pessoas;
+
+        System.out.print("=====================");
+        System.out.printf("\nMédia da idade dos participantes: %.2f",mediaidade);
+        System.out.printf("\nMédia de altura: %.2f\nMédia de peso: %.2f",medialtura,mediapeso);
+        System.out.printf("\nPorcentagem de mulheres: %.2f\nPorcentagem de homens: %.2f",porcentfem, porcentmasc);
+        System.out.printf("\nNúmero de pessoas ruivas de olhos azuis: %d",azulruivo);
     }
 }
