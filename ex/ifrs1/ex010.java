7ae81b5c893670e023cb177bd52b8a5fc5dfcd9e
caio-sdm <marinhocaio1308@gmail.com>
2025-06-12 09:41:38 -0300

    ex010

--- ex/ifrs1/ex010.java
+++ ex/ifrs1/ex010.java
@@ -3,7 +3,36 @@ package ex.ifrs1;
 public class ex010 {
     public static void main (String[]args){
         int numero;
+        boolean primo;
+        char continuar;
 
-        if ()
+        System.out.print("Inserir número? (S/N)");
+        continuar = System.console().readLine().charAt(0);
+
+        while (continuar == 's' || continuar == 'S') {
+            primo = true;
+            System.out.print("Digite um número maior que 1: ");
+            numero = Integer.parseInt(System.console().readLine());
+            if (numero <= 1) {
+                System.out.print("Digite um número maior que 1!");
+                break;
+            } else {
+                for (int i = 2; i < numero; i++) {
+                    if (numero % i == 0) {
+                        primo = false;
+                        break;
+                    }
+                }
+
+                if (primo) {
+                    System.out.printf("%d é primo", numero);
+                } else {
+                    System.out.printf("%d não é primo", numero);
+                }
+            }
+            System.out.print("\nInserir outro número? (S/N)");
+            continuar = System.console().readLine().charAt(0);
+        }
+        System.out.print("\n------------------\nPrograma Finalizado :D");
     }
 }
