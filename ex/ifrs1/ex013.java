5328afc87ebbc71e15de188f3d5831a8b81e2d0c
caio-sdm <marinhocaio1308@gmail.com>
2025-06-12 15:34:49 -0300

    ex013 inicio

---
+++ ex/ifrs1/ex013.java
@@ -0,0 +1,9 @@
+package ex.ifrs1;
+/*dado um valor 'bits', quantidades de cédulas para esse valor; caixa começa com 100 notas de cada; valor máximo por saque: 1000; termina ao digitar negativo ou limite de saques (100) */
+
+public class ex013 {
+    public static void main (String[]args){
+        int valorbits, notas50, notas10, notas5, notas1, saques;
+
+    }
+}
