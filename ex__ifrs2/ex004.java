4b35f82e1e432c72657c2cceb58fd414aceb1be4
caio-sdm <marinhocaio1308@gmail.com>
2025-06-13 11:38:46 -0300

    ex004 inicio

---
+++ ex__ifrs2/ex004.java
@@ -0,0 +1,11 @@
+package ex__ifrs2;
+
+public class ex004 {
+    public static void main (String[]args){
+        int[] a = new int[5], b = new int[5];
+
+        for (int i = 0; i < 5; i++){
+            
+        }
+    }
+}
