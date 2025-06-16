1d8c8475af6b951c211d7da194584d2ca6482394
caio-sdm <marinhocaio1308@gmail.com>
2025-06-15 22:12:49 -0300

    ex003

--- ex__ifrs4/ex003.java
+++ ex__ifrs4/ex003.java
@@ -1,4 +1,22 @@
 package ex__ifrs4;
 
 public class ex003 {
+    public static boolean primo(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main (String[]args){
+        System.out.print("Numeros primos, de 1 a 100: \n");
+        for (int i = 1; i <= 100; i++){
+            if (primo(i)){
+                System.out.print(i + " ");
+            }
+        }
+        System.out.print("\n");
+    }
 }
